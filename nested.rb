cee88116eb7c5a036d728d935ae79d86ad66d8c0
nikila-saravanan <nikila.saravanan@gmail.com>
2015-07-30 17:33:01 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
         }
      }
 
+  hopper_value = programmer_hash[:grace_hopper]
+  hopper_value
 
 end
 
@@ -37,6 +39,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+    known_for = programmer_hash[:alan_kay][:known_for]
+    known_for
 end
 
 def dennis_ritchies_language
@@ -55,6 +60,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    ritchie_lang = programmer_hash[:dennis_ritchie][:languages][0]
+    ritchie_lang
+
 end
 
 def adding_matz
@@ -79,7 +87,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
 
+  programmer_hash
     
 end
 
@@ -101,6 +114,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+
+     programmer_hash
      
      
 end
@@ -121,7 +137,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
      
 end
 
